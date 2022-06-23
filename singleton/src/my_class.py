408fbbb23ad6fa64da111e94c61acad285e590bd
pedropeglow <pedropeglowm@gmail.com>
2022-06-23 20:34:58 -0300

    :bug: Aplicando Design Pattern Singleton

---
+++ singleton/src/my_class.py
@@ -0,0 +1,11 @@
+class SaySomething:
+
+    def __init__(self, message):
+        self.message = message
+
+    def say(self):
+        print(self.message)
+        print(hex(id(self.message)))
+
+    def change_message(self, new_message):
+        self.message = new_message
